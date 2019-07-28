bb2fff6af08786281c96a458717ee190aa31da46
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-28 15:26:40 -0600

    Adds router to root and main component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Harvard Art Museum Explorer</h1>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
