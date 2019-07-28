273659ab8f8c01e8bb4578f6773568492fef30d4
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-28 17:04:41 -0600

    Adds data attributes and lifecycle methods

--- src/components/FloorContainer.vue
+++ src/components/FloorContainer.vue
@@ -1,12 +1,37 @@
 <template>
   <main>
-    <h2>Floor{{ $route.params.floor_number }}</h2>
+    <h2>Floor{{ this.currFloor }}</h2>
   </main>
 </template>
 
 <script>
-export default {
+import GalleriesBar from './GalleriesBar';
 
+export default {
+  name: 'FloorContainer',
+  components: {
+    GalleriesBar
+  },
+  data() {
+    return {
+      galleries: [],
+      currFloor: this.$route.params.floor_number
+    }
+  },
+  async created() {
+    try {
+      const apiKey = process.env.VUE_APP_API_KEY;
+      
+    } catch(error) {
+      console.log(error); 
+    }
+  },
+  watch:{
+    $route (to, from){
+      const { floor_number } = to.params;
+      this.currFloor = floor_number;
+    }
+  } 
 }
 </script>
 
---
+++ src/components/GalleriesBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <aside>
+    <nav>
+      <ul>
+        <p>HI</p>
+      </ul>
+    </nav>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'GalleriesBar'
+}
+</script>
+
+<style>
+
+</style>
