fff02fa40d63c61193b3fd698b83df0453a7ecf3
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-28 18:26:24 -0600

    Removes gallery button component

--- src/components/GalleriesBar.vue
+++ src/components/GalleriesBar.vue
@@ -1,28 +1,37 @@
 <template>
   <aside>
     <nav>
-      <ul v-for="gallery in galleries" v-bind:key="gallery.galleryid">
-        <GalleryButton 
-        v-bind:theme="gallery.theme" 
-        v-bind:id="gallery.galleryid"
-        v-bind:name="gallery.name" />
+      <ul>
+        <li v-for="gallery in galleries" v-bind:key="gallery.galleryid">
+          {{ gallery.theme || gallery.name }}
+        </li>
       </ul>
     </nav>
   </aside>
 </template>
 
 <script>
-import GalleryButton from './GalleryButton'
-
 export default {
   name: 'GalleriesBar',
-  components: {
-    GalleryButton
-  },
   props: ['galleries']
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+    aside {
+      width: 25%;
 
+      ul {
+        margin: 0;
+        padding: 10px;
+
+        li {
+          list-style-type: none;
+          text-align: left;
+          padding: 10px;
+          margin: 0;
+          font-size: 1.2rem;
+        }
+      }
+    }
 </style>
