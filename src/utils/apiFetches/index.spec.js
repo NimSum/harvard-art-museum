d3dab5d002370237dad56c50f36d7c7a198b753f
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-30 14:49:01 -0600

    Tests data return, bad requests/response

--- src/utils/apiFetches/index.spec.js
+++ src/utils/apiFetches/index.spec.js
@@ -6,13 +6,25 @@ jest.mock("axios");
 
 describe('getAnything', () => {
   const mockUrl = apiUrls.galleriesByFloor(1);
-  const mockResponse = {
-    statusText: "OK"
-  }
+
+  it('should return fetched data', async () => {
+    axios.get.mockImplementation(() => Promise.resolve({ data: ["Fake Data"], statusText: "OK" }));
+    const result = await getAnything(mockUrl);
+    expect(result).toEqual(["Fake Data"]);
+  })
 
   it('should fetch with the correct params', async () => {
-    axios.get.mockImplementation(() => Promise.resolve(mockResponse));
+    axios.get.mockImplementation(() => Promise.resolve({ statusText: "OK" }));
     await getAnything(mockUrl);
     expect(axios.get).toHaveBeenCalledWith(mockUrl);
+  })
+
+  it('should throw an error if fetch fails', async () => {
+    axios.get.mockImplementation(() => Promise.resolve({ statusText: "BAD"}));
+    try {
+      await getAnything(mockUrl)
+    } catch(error) {
+      expect(error).toEqual(Error('Failed to fetch data'));
+    }
   })
 })
